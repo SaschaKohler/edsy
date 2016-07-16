6f39ec18484765b95cce9b1a9a2df7765ae9a72d
Sascha Kohler <sascha.kohler@sent.at>
2016-07-16 22:55:16 +0200

    Add validations for Users and Listings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+    	user_params.permit(:name, :email, :password)
+	end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+    	user_params.permit(:name, :email , :current_password)
+	end
+  end
+
+
 end
